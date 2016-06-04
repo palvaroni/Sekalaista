9e5883748cb85cabceb807e7dd9aae3d202af462
Roni Palva-aho <roni.palvaaho@gmail.com>
2016-06-04 13:19:39 +0300

    Add files via upload

---
+++ gridsystem/index.php
@@ -0,0 +1,35 @@
+<div id="index-gridwrapper" class="gridwrapper">
+	<div id="index-leftwrapper" class="w6">
+		<div id="index-left" class="h10">
+			<a href="<?php echo get_lookbook_newest_url(); ?>">
+			<div class="overlay"></div>
+			<div style="background-image:url('<?php echo get_template_directory_uri(); ?>/img/IMG_8641_1000.jpg')">
+				<h2 class="bottom">Otsikko</h2>
+			</div>
+			</a>
+		</div>
+	</div>
+
+	<div id="index-rightwrapper" class="w8">
+		<div id="index-mid-top" class="h5">
+			<div style="background-image:url('<?php echo get_template_directory_uri(); ?>/img/IMG_9158-4_1000.jpg')"></div>
+		</div>
+		<div id="index-left-bottom" style="padding-left:0" class="h5 w7">
+			<a href ="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) ); ?>">
+			<div class="overlay"></div>
+			<div style="background-image:url('<?php echo get_template_directory_uri(); ?>/img/IMG_8462a_1000.jpg')">
+				<h2>Otsikko</h2>
+			</div>
+			</a>
+		</div>
+		
+		<div id="index-right-bottom" style="padding-right:0" class="h5 w7">
+			<a href ="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) ); ?>/?orderby=date">
+			<div class="overlay"></div>
+			<div style="background-image:url('<?php echo get_template_directory_uri(); ?>/img/IMG_8616_1000.jpg">
+				<h2>Otsikko</h2>
+			</div>
+			</a>
+		</div>
+	</div>
+</div>
