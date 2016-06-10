eedc7fd15d8a03e8c3c937e4b90972ed10d0deef
Roni Palva-aho <roni.palvaaho@gmail.com>
2016-06-10 11:50:50 +0300

    Create dumbSorter.php

---
+++ dumbSorter.php
@@ -0,0 +1,50 @@
+<?php
+
+$currentChar = 'A';
+$prevIndex = 0;
+$currIndex = -1;
+$lastIndex = count($nameArr) - 1;
+
+while ($currIndex < $lastIndex) {
+    foreach ($nameArr as &$name) {
+        if (strtoupper($name[0]) === $currentChar) {
+            $sortedArr[] = $name;
+            unset($name);
+        }
+    }
+
+    $currIndex = count($sortedArr) - 1;
+
+    for ($prevIndex; $prevIndex < $currIndex; $prevIndex++) {
+        $i = $prevIndex + 1;
+
+        while($i < $currIndex) {
+            $c = 1;
+            while (true) {
+                if (!isset($sortedArr[$prevIndex][$c])) {
+                    break;
+                } elseif (!isset($sortedArr[$i][$c])) {
+                    $temp = $sortedArr[$i];
+                    $sortedArr[$i] = $sortedArr[$prevIndex];
+                    $sortedArr[$prevIndex] = $temp;
+                    break;
+                } elseif (strtoupper($sortedArr[$prevIndex][$c]) === strtoupper($sortedArr[$i][$c])) {
+                    $c++;
+                } elseif (strtoupper($sortedArr[$prevIndex][$c]) > strtoupper($sortedArr[$i][$c])) {
+                    $temp = $sortedArr[$i];
+                    $sortedArr[$i] = $sortedArr[$prevIndex];
+                    $sortedArr[$prevIndex] = $temp;
+                    break;
+                } elseif (strtoupper($sortedArr[$prevIndex][$c]) < strtoupper($sortedArr[$i][$c])) {
+                    break;
+                } else {
+                    die('ODOTTAMATON TILA');
+                }
+            }
+            $i++;
+        }
+    }
+
+    $prevIndex++;
+    $currentChar++;
+}
