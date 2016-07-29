8fc11249c2234ef878ce2717b6523c65c1b5ed39
Roni Palva-aho <roni.palvaaho@gmail.com>
2016-07-29 07:36:04 +0300

    Update menuwalker.js

--- menuwalker.js
+++ menuwalker.js
@@ -1,23 +1,23 @@
-	function generateInfoMenu(){
-		var $ = jQuery;
-		// /*
-		var linkList = $("#sidebar").prepend('<ul class="infolink-ul"></ul>').find('ul');
-		var m = window.location.href.match(/#.+/);
-		
-		if ( m !== null ) {
-			$(m[0]).addClass('active');
-		}
-		
-		$('.infotag').each(function(index){			
-			var target = $(this).attr('id'); // target div
-			var text = $(this).children('h3').text(); // text content for display
-			
-			linkList.append('<li><a href="#'+target+'">'+text+'</a></li>');
-		});
-		
-		linkList.find('a').on('click', function(){
-			var id = $(this).attr('href');
-			$(id).addClass('active');
-		});
-		// */		
+function generateInfoMenu(){
+	var $ = jQuery;
+	// /*
+	var linkList = $("#sidebar").prepend('<ul class="infolink-ul"></ul>').find('ul');
+	var m = window.location.href.match(/#.+/);
+	
+	if ( m !== null ) {
+		$(m[0]).addClass('active');
 	}
+	
+	$('.infotag').each(function(index){			
+		var target = $(this).attr('id'); // target div
+		var text = $(this).children('h3').text(); // text content for display
+		
+		linkList.append('<li><a href="#'+target+'">'+text+'</a></li>');
+	});
+	
+	linkList.find('a').on('click', function(){
+		var id = $(this).attr('href');
+		$(id).addClass('active');
+	});
+	// */		
+}
