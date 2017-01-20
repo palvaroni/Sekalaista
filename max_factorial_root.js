4f4ab57e18a764469033e0e9c746ea21500592f8
Roni Palva-aho <roni.palvaaho@gmail.com>
2017-01-20 20:41:39 +0200

    Create max_factorial_root.js

---
+++ max_factorial_root.js
@@ -0,0 +1,15 @@
+/**
+ * Return maximum int for which it's
+ * factorial is equal to or smaller than
+ * the given value n.
+ */
+function max_factorial_root(n) {
+	let carry = n,
+		z = 1;
+		
+	while(z < carry) {
+		if (carry === z) break;
+		carry /= ++z;
+    }
+	return z;
+}
