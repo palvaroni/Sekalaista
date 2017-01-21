4be37fe57f83a6afdc317b673583b5d3b28a90dc
Roni Palva-aho <roni.palvaaho@gmail.com>
2017-01-21 10:25:45 +0200

    Update max_value_nlog2n.js

--- max_value_nlog2n.js
+++ max_value_nlog2n.js
@@ -1,5 +1,5 @@
 /**
- * Return maximum value for parameter with
+ * Return maximum int value for parameter with
  * which an algorithm of complexity O(n log2 n)
  * can be evaluated in the given amount of
  * time units n.
@@ -7,7 +7,7 @@
  * Using this Lambert W implementation
  * https://github.com/protobi/lambertw
  */
-function max_value_nlog2n(n) {
+function max_param_nlog2n(n) {
 	// Math.log(x) == ln x
 	return Math.pow(Math.E, gsl_sf_lambert_W0(n*Math.log(2))) >>> 0;
 }
